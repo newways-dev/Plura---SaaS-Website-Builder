ba9d24a92cc4b2167089188b2851f0f74e2b5fea
Kate <kate.dew.next@gmail.com>
2025-09-07 23:02:59 +0200

    notification query

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,4 +1,18 @@
-const Page = () => {
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+  const authUser = await currentUser()
+
+  if (!authUser) return redirect('/sign-in')
+
+  const agencyId = await verifyAndAcceptInvitation()
+  console.log(agencyId)
+
+  //get the users details
+  const user = await getAuthUserDetails()
+
   return <div>Agency Page</div>
 }
 
